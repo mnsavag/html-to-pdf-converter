cc41a70e17d47c30f45014f1b1441cae9a95b100
mnsavag <mnsavag@gmail.com>
2024-01-30 16:47:52 +0500

    mvp

--- app/middleware/errorHandler.middleware.js
+++ app/middleware/errorHandler.middleware.js
@@ -3,7 +3,7 @@ import { logger } from "../utils/loggers.utils.js"
 
 export const errorHandler = (error, req, res, next) => {
     logger.error(error) // info() ???
-
+    
     if (error.status != 500) {
         res.status(error.status).json({
             name: error.name,
